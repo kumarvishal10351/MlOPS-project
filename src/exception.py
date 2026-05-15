8e1ab6fe2afda886806d000bba2248b4565a34a5
kumarvikash10351 <kumarvikash10351@gmail.com>
2026-05-16 02:00:43 +0530

    ingestion added

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,8 @@
 import sys
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
